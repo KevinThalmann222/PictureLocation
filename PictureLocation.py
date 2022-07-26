1cb5f4d81f5fd8ee6168c7b2fd3a82932b6f21bb
Kevin Thalmann <kevin_thalmann@yahoo.de>
2022-07-26 15:01:18 +0200

    Erste Änderungen

---
+++ PictureLocation.py
@@ -0,0 +1,90 @@
+import PIL.Image
+import PIL.ExifTags
+import gmplot
+from geopy.geocoders import Nominatim
+import webbrowser
+
+
+class PictureLocation:
+    def __init__(self, picture):
+        """_summary_
+
+        Args:
+            picture (_type_): _description_
+        """
+        self.picture = picture
+        img = PIL.Image.open(self.picture)
+        exif = {PIL.ExifTags.TAGS[key]: value for key, value in img._getexif().items() if key in PIL.ExifTags.TAGS}
+        self.coordinations = exif.get("GPSInfo")
+
+        self.latitude_coor_dg = None
+        self.longitude_coor_dg = None
+
+    def get_coordinaten(self, coor="dg"):
+        """_summary_
+
+        Args:
+            coor (str, optional): _description_. Defaults to "dg".
+
+        Raises:
+            RuntimeError: _description_
+
+        Returns:
+            _type_: _description_
+        """
+        latitude_directory = self.coordinations[1]
+        longitude_directory = self.coordinations[3]
+
+        latitude_coor_gms = self.coordinations[2]
+        longitude_coor_gms = self.coordinations[4]
+
+        self.latitude_coor_dg = (int(latitude_coor_gms[2]) / 60 + int(latitude_coor_gms[1])) / 60 + int(latitude_coor_gms[0])
+        self.longitude_coor_dg = (int(longitude_coor_gms[2]) / 60 + int(longitude_coor_gms[1])) / 60 + int(longitude_coor_gms[0])
+
+        if latitude_directory == "S":
+            self.latitude_coor_dg *= -1
+        if longitude_directory == "W":
+            self.longitude_coor_dg *= -1
+
+        if coor == "dg":
+            return (self.latitude_coor_dg, self.longitude_coor_dg)
+        elif coor == "gms":
+            return (latitude_coor_gms, longitude_coor_gms)
+        else:
+            raise RuntimeError("Please choose dg (Dezimalgrad) or gms (Grad, Minuten, Sekunden)")
+
+    def get_address(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        if not self.latitude_coor_dg or not self.longitude_coor_dg:
+            self.latitude_coor_dg, self.longitude_coor_dg = self.get_coordinaten(coor="dg")
+        geo_loc = Nominatim(user_agent="GetLoc")
+        loc_name = geo_loc.reverse(f"{self.latitude_coor_dg}, {self.longitude_coor_dg}").address
+        print(f"the location of the picture '{self.picture}' is:")
+        print(f"         --> {loc_name}")
+
+        return loc_name
+
+    def creat_map(self, openbrowser=True, zoom=15):
+        """_summary_
+
+        Args:
+            openbrowser (bool, optional): _description_. Defaults to True.
+            zoom (int, optional): _description_. Defaults to 15.
+        """
+        if not self.latitude_coor_dg or not self.longitude_coor_dg:
+            self.latitude_coor_dg, self.longitude_coor_dg = self.get_coordinaten(coor="dg")
+        gmap = gmplot.gmplot.GoogleMapPlotter(self.latitude_coor_dg, self.longitude_coor_dg, zoom)
+        gmap.marker(self.latitude_coor_dg, self.longitude_coor_dg, "cornflowerblue")
+        gmap.draw("location.html")
+        if openbrowser:
+            webbrowser.open("location.html", new=2)
+
+
+if __name__ == "__main__":
+    pic_loc = PictureLocation("Test1.jpg")
+    # pic_loc.get_address()
+    pic_loc.creat_map()
